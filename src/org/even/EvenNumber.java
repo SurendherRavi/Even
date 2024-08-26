b4765bacd279b31b5f77f2b3e8e3670eee0162d5
SurendherRavi <surendherstech05@gmail.com>
2024-08-26 11:52:59 +0530

    Hi

---
+++ src/org/even/EvenNumber.java
@@ -0,0 +1,17 @@
+package org.even;
+
+public class EvenNumber {
+	public void countEven(int num) {
+		int count = 0;
+		for (int i=1; i<=100; i++) {
+			if(i % 2 == 0) {
+			 count++;
+			}
+			}
+		System.out.println(count);
+	}
+	public static void main(String[] args) {
+		EvenNumber e = new EvenNumber();
+		e.countEven(0);
+	}
+}
